ec3e1431fa8916fe79e217939f2697ddb5fef290
cx3576888 <cx3576888@gmail.com>
2021-11-22 04:33:23 +0800

    week 2 preliminary

--- src/week2.js
+++ src/week2.js
@@ -1,11 +1,222 @@
+import jsSHA from 'jssha';
+import axios from 'axios';
+import { createApp } from 'vue/dist/vue.esm-browser';
 import L from '../node_modules/leaflet/dist/leaflet';
 
-const mymap = L.map('map').setView([51.505, -0.09], 13);
+// https://github.com/ptxmotc/Sample-code/blob/master/Node.js/sample.js
+const getAuthorizationHeader = function () {
+  const AppID = 'da4c52a5c3144c54a69a6d4f40697fb6';
+  const AppKey = 'w2jtXxrjCFl2wN5RWmJO6HOjM38';
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(mymap);
+  const GMTString = new Date().toGMTString();
+  const ShaObj = new jsSHA('SHA-1', 'TEXT');
+  ShaObj.setHMACKey(AppKey, 'TEXT');
+  ShaObj.update('x-date: ' + GMTString);
+  const HMAC = ShaObj.getHMAC('B64');
+  const Authorization = `hmac username="${AppID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
 
+  return { 'Authorization': Authorization, 'X-Date': GMTString };
+}
+
+const bikeApi = axios.create({
+  baseURL: 'https://ptx.transportdata.tw/MOTC/v2/Bike',
+  headers: getAuthorizationHeader(),
+});
+
+const getStations = function (by) {
+  let url;
+  const params = {
+    $top: 5,
+    $format: 'JSON',
+  };
+  if (by.city) {
+    url = `/Station/${by.city}`;
+  } else {
+    url = `/Station/NearBy`;
+    params.$spatialFilter = `nearby(${by.lat},${by.lng},${by.distance})`;
+  }
+  return bikeApi.get(url, { params: params });
+}
+
+const getAvailability = function (by) {
+  let url;
+  const params = {
+    $top: 5,
+    $format: 'JSON',
+  };
+  if (by.city) {
+    url = `/Availability/${by.city}`;
+  } else {
+    url = `/Availability/NearBy`;
+    params.$spatialFilter = `nearby(${by.lat},${by.lng},${by.distance})`;
+  }
+  return bikeApi.get(url, { params: params });
+}
+
+function getUserLocation() {
+  const p = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // console.log('user position:', position);
+        resolve(position);
+      },
+      (err) => {
+        console.log('get user position error', err);
+        reject(err);
+      }
+    )
+  });
+  return p;
+}
+
+const icons = prepareIcons();
+const blueIcon = icons.blue;
+const grayIcon = icons.gray;
+const markers = {};
+const app = createApp({
+  data() {
+    return {
+      map: {},
+      currPosition: {
+        lat: null,
+        lng: null,
+      },
+      isBorrow: true,
+      showDetail: false,
+      name: {},
+      address: {},
+      updateTime: '',
+      status: {},
+      available: {},
+    };
+  },
+  mounted() {
+    getUserLocation()
+      .then((position) => {
+        this.currPosition.lat = position.coords.latitude;
+        this.currPosition.lng = position.coords.longitude;
+      })
+      .catch((err) => {
+        console.log('use Taipei City Hall Stataion as default user location');
+        this.currPosition.lat = 25.0408578889;
+        this.currPosition.lng = 121.567904444;
+      })
+      .finally(() => {
+        this.map = constructMap(this.currPosition);
+      })
+  },
+  methods: {
+    toggleBorrow() {
+      this.isBorrow = !this.isBorrow;
+      markStations(this.map, this.isBorrow, this);
+    },
+  },
+});
+app.mount('#app');
+
+function constructMap(currPosition) {
+  map = L.map('map', {
+    center: currPosition,
+    zoom: 17,
+  });
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+  return map;
+}
+
+function prepareIcons() {
+  const bearIcon = L.Icon.extend({
+    options: {
+      iconSize: [60, 60],
+      iconAnchor: [30, 60],
+      popupAnchor: [0, -60]
+    }
+  });
+  return {
+    blue: new bearIcon({ iconUrl: require('../assets/bear-blue.png') }),
+    gray: new bearIcon({ iconUrl: require('../assets/bear-gray.png') }),
+  };
+}
+
+function markStations(map, isBorrow, vueObj) {
+  const byParam = {
+    lat: map.getCenter().lat,
+    lng: map.getCenter().lng,
+    distance: 1000,
+  };
+  Promise.all([getStations(byParam), getAvailability(byParam)])
+    .then(([stations, avails]) => {
+      stations.data.forEach(st => {
+        markers[st.StationUID] = L.marker([st.StationPosition.PositionLat, st.StationPosition.PositionLon], {
+          icon: isBorrow ? blueIcon : grayIcon,
+        })
+          .addTo(map)
+          .on('click', function (evt) {
+            showStationDetails(evt, st, vueObj);
+          });
+      });
+
+      avails.data.forEach(st => {
+        const stMarker = markers[st.StationUID];
+        if (stMarker) {
+          stMarker
+            .bindTooltip(isBorrow ? '' + st.AvailableRentBikes : '' + st.AvailableReturnBikes, {
+              permanent: true,
+              direction: 'center',
+              offset: [-3, -33],
+              className: isBorrow ? 'borrow-num' : 'return-num',
+            })
+            .openTooltip()
+            .on('click', function (evt) {
+              showAvailableDetails(evt, st, vueObj);
+            });
+        } else {
+          console.log('no marker for ', st.StationUID)
+        }
+      })
+    })
+}
+
+function showStationDetails(evt, st, vueObj) {
+  vueObj.showDetail = true;
+  vueObj.name = st.StationName;
+  vueObj.address = st.StationAddress;
+  vueObj.updateTime = timeFormat(st.UpdateTime);
+}
+
+function showAvailableDetails(evt, st, vueObj) {
+  vueObj.status = updateStatus(st.ServiceStatus);
+  vueObj.available.borrow = st.AvailableRentBikes;
+  vueObj.available.return = st.AvailableReturnBikes;
+}
+
+function timeFormat(time) {
+  const idx1 = time.indexOf('T');
+  const idx2 = time.indexOf('+');
+  return time.substring(0, idx1) + ' ' + time.substring(idx1 + 1, idx2);
+}
+
+function updateStatus(statusId) {
+  const ret = {};
+  switch (statusId) {
+    case 0:
+      ret.text = '停止營運';
+      ret.color = 'ball-red';
+      break;
+    case 1:
+      ret.text = '正常營運';
+      ret.color = 'ball-blue';
+      break;
+    case 2:
+      ret.text = '暫停營運';
+      ret.color = 'ball-yellow';
+      break;
+  }
+  return ret;
+}
+
+/*
 const circle = L.circle([51.508, -0.11], {
   color: 'red',
   fillColor: '#f03',
@@ -68,3 +279,4 @@ const marker2 = L.marker([51.495, -0.087], { icon: markerIcon }).addTo(mymap);
 // const marker = L.marker([51.5, -0.09]).addTo(mymap); // this won't work because don't know where the png is after build
 marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
 marker2.bindPopup('I am a popup, too.');
+*/
